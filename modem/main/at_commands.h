b1f3dcd86ec85c1ab29171084062422b9a4ac957
LincolnCMJ <lincoln.ifes@gmail.com>
2023-10-07 19:13:17 -0300

    Inserir
    Programação para comunicação MQTT.

--- modem/main/at_commands.h
+++ modem/main/at_commands.h
@@ -21,10 +21,9 @@ void at_messages()
         "AT+SAPBR=2,1\r\n",
         "AT+HTTPINIT\r\n",
         "AT+HTTPPARA=\"CID\",1\r\n",
-        "AT+CIPSTART=\"TCP\",\"exploreembedded.com\",80\r\n",
-        "AT+CIPSEND=63\r\n",
-        "GET exploreembedded.com/wiki/images/1/15/Hello.txt HTTP/1.0\r\n",
-        "\x1A",
+        "AT+HTTPPARA=\"URL\",\"https://www.m2msupport.net/m2msupport/http_get_test.php\"\r\n",
+        "AT+HTTPDATA=16,10000\r\n",
+        "AT+HTTPACTION=1\r\n"
     };
     int num_messages = sizeof(messages) / sizeof(messages[0]);
     int current_message_index = 0;
@@ -40,6 +39,4 @@ void at_messages()
         receive();
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    receive();
 }
